748c7b5c2d303a6d37d1c26e1602b7d4add822a6
Andrew Antolino <andrew@smithbrothersmedia.com.au>
2019-08-27 23:45:43 +1000

    Add spinner for async loading

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -8,7 +8,7 @@ class Modal extends Component {
   // The component should only update if it is being shown
   // i.e. if the `show` prop is true
   shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.show !== this.props.show;
+    return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
   }
 
   componentWillUpdate() {
