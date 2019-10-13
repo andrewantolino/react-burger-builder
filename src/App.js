b2627a62f3f7c4b1ac145422f6bb193f225c8be9
Andrew Antolino <andrew@smithbrothersmedia.com.au>
2019-10-14 00:04:39 +1100

    Test removal of axios interceptors

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 import classes from './App.css';
 
-function App() {
-  return (
-    <div className={classes.App}>
-      <Layout>
-        <BurgerBuilder />
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  state = {
+    show: true
+  };
+
+  render() {
+    return (
+      <div className={classes.App}>
+        <Layout>
+          { this.state.show ? <BurgerBuilder /> : null }
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
