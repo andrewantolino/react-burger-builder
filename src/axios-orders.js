93ecb82c970a83d75b517b57cd1b62bfaa852773
Andrew Antolino <andrew@smithbrothersmedia.com.au>
2020-02-24 23:04:18 +1100

    Update firebase project URL

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://react-burger-builder-102b5.firebaseio.com/'
+  baseURL: 'https://react-burger-builder-652f2.firebaseio.com/'
 });
 
 export default instance;
